1e6bc7a5384d492b57c26c59083931aabf865e87
Nicolai Marlon Schirmer <xnschsch957@ieshnosmachado.org>
2025-02-22 20:15:52 +0100

    Delete app directory

--- app/src/main/java/com/nicolaischirmer/proyectodb/firebase/model/WeaponsDB.kt
+++
@@ -1,9 +0,0 @@
-package com.nicolaischirmer.proyectodb.firebase.model
-
-data class WeaponsDB(
-    val userId: String = "",
-    val name:  String = "",
-    val type: String = "",
-    val description: String = "",
-    val damage: Int = 0
-)
